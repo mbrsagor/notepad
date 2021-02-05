a8ce08b78578d867ed7645edc2efd5cf4c9aaaa6
mbrsagor <mbrsagor@gmail.com>
2021-02-06 00:14:56 +0600

    Shoping cart basic setup

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+import 'react-bootstrap/dist/react-bootstrap.js';
+import './css/style.scss';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
