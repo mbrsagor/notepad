640ab13d9229e8567fc1a97e17c49e8497b653c4
mbrsagor <mbrsagor@gmail.com>
2021-02-05 12:59:13 +0600

    Refactor and inital commit

--- src/component/about/About.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-
-class About extends Component {
-    render() {
-        return <h1>About my story Please, wait comming the story!</h1>
-    }
-}
-
-export default About
--- src/component/stateLess/SateLessComponent.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const SateLessComponent = (props) => {
-    return (
-        <h1>Hello there, I am {props.name}</h1>
-    )
-}
-
-export default SateLessComponent;
