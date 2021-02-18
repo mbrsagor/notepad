f657f1ea376288fe986e7a274c789f8d152b8176
mbrsagor <mbrsagor@gmail.com>
2021-02-19 01:00:37 +0600

    Refactor

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
 import App from './App';
-import 'react-bootstrap/dist/react-bootstrap.js';
-import './css/style.scss';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
