eeddcb3270aacb3b2702e4a1f08df1cbb540d657
mbrsagor <mbrsagor@gmail.com>
2021-04-29 01:30:24 +0600

    Add project ID

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import { ChatEngine } from 'react-chat-engine';
 import './App.css';
+
 
 const App = () =>{
     return(
-        <div>
-            <h2>Hello Chat App</h2>
-        </div>
+        <ChatEngine
+            publicKey='16c99131-aaf4-4ea0-abd2-a9e77b063e30'
+            userName='demo'
+            userSecret='admin'
+            height='100vh'
+            projectID="3705bd1e-23d8-4290-af3a-25797c7ecdb5"
+        />
     )
 }
 
