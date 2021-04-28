26c55073a9a9f3542320ff79c9ef892abcf146ba
mbrsagor <mbrsagor@gmail.com>
2021-04-29 01:02:14 +0600

    Inital commit for refactor

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+const App = () =>{
+    return(
+        <div>
+            <h2>Hello Chat App</h2>
+        </div>
+    )
+}
+
+export default App;
