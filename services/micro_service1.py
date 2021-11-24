ddabc71b5e97e238c7c2a45b672c472a479ef063
mbrsagor <mbrsagor@gmail.com>
2021-11-24 13:27:33 +0600

    New: Service worker basic setup

---
+++ services/micro_service1.py
@@ -0,0 +1,40 @@
+def jon_service():
+    {
+    "saurabh_badhwar": {
+        "id":1,
+        "name":"Saurabh Badhwar",
+        "verified":1
+    },
+    "aniket": {
+        "id":2,
+        "name":"Aniket Sharma",
+        "verified":1
+    },
+    "luckas": {
+        "id":4,
+        "name":"Luckas Friendel",
+        "verified":0
+    }
+}
+
+
+def jon_service_v2():
+    {
+    "saurabh_badhwar": {
+        "home": [
+            "Buy milk",
+            "Look for pest control service",
+            "Get a new carpet"
+        ],
+        "work": [
+            "Complete the blogpost",
+            "Create presentation for meeting"
+        ]
+    },
+    "aniket": {
+        "school": [
+            "Complete homework",
+            "Prepare for test"
+        ]
+    }
+}
