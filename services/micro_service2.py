337bc47c966df7719d920e2212796db0b44a2f4a
mbrsagor <mbrsagor@gmail.com>
2022-03-11 20:47:56 +0600

    New: Add hero json

---
+++ services/micro_service2.py
@@ -0,0 +1,18 @@
+def jon_service():
+    {
+    "saurabh_badhwar": {
+        "id":1,
+        "name":"Saurabh Badhwar",
+        "verified":1
+    },
+    "aniket": {
+        "id":2,
+        "name":"Aniket Sharma",
+        "verified":1
+    },
+    "luckas": {
+        "id":4,
+        "name":"Luckas Friendel",
+        "verified":0
+    }
+}
