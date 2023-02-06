0e709972bf64a3dc747ade5b32cac738a65292c0
mbrsagor <mbrsagor@gmail.com>
2023-02-06 13:28:05 +0600

    New: Hello world static API.

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
 from flask import Flask
 
+from flask_restful import Resource, Api
+
 app = Flask(__name__)
+api = Api(app)
 
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+class Welcome(Resource):
+    def get(self):
+        return {"data": "Hello there, this is Mbr Sagor"}
 
+
+api.add_resource(Welcome, '/')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
