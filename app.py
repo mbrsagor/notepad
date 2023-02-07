91146cefa422eeb51b8a273c5d560c9bdfa9df26
mbrsagor <mbrsagor@gmail.com>
2023-02-07 15:15:26 +0600

    New: Notepad details, update, and delete API done.

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_restful import Resource, Api
 
-from messages import SUCCESS_MSG, ERROR_MSG, DATA_RENDER_SUCCESS, DATA_RENDER_FAIL
+from messages import SUCCESS_MSG, ERROR_MSG, DATA_RENDER_SUCCESS, DATA_RENDER_FAIL, NOTE_UPDATE_MSG, NOTE_DELETE_MSG
 
 app = Flask(__name__)
 api = Api(app)  # For API service
@@ -54,14 +54,63 @@ class CreateNoteAPIView(Resource):
         return self.response, 201
 
 
-class GetNameAPI(Resource):
-    def get(self, name):
-        return {"data": f"Hello {name}"}
+class GetNoteByIDAPIView(Resource):
+    response = {"status": 404, "message": DATA_RENDER_SUCCESS}
+
+    def get(self, note_id):
+        note = NoteModel.query.filter_by(id=note_id).first()
+        if note:
+            notepad_details = {
+                'id': note.id,
+                'title': note.title,
+                'description': note.description,
+            }
+            self.response["status"] = 200
+            self.response["data"] = notepad_details
+            return self.response, 200
+        else:
+            return self.response, 404
+
+
+class NoteUpdateAPIView(Resource):
+    response = {"status": 404, "message": ERROR_MSG}
+
+    def put(self, note_id):
+        note = NoteModel.query.filter_by(id=note_id).first()
+        if note:
+            data = request.get_json()
+            title = data['title']
+            description = data['description']
+            note.title = title
+            note.description = description
+            db.session.commit()
+            self.response["status"] = 200
+            self.response["message"] = NOTE_UPDATE_MSG
+            return self.response, 200
+        else:
+            return self.response, 404
+
+
+class DeleteNoteAPIView(Resource):
+    response = {"status": 404, "message": ERROR_MSG}
+
+    def delete(self, note_id):
+        note = NoteModel.query.filter_by(id=note_id).first()
+        if note:
+            db.session.delete(note)
+            db.session.commit()
+            self.response["status"] = 200
+            self.response["message"] = NOTE_DELETE_MSG
+            return self.response, 200
+        else:
+            return self.response, 404
 
 
 api.add_resource(NotePadAPIList, '/')
 api.add_resource(CreateNoteAPIView, '/create/')
-api.add_resource(GetNameAPI, '/hello/<string:name>/')
+api.add_resource(GetNoteByIDAPIView, '/note/<int:note_id>/')
+api.add_resource(NoteUpdateAPIView, '/note-update/<int:note_id>/')
+api.add_resource(DeleteNoteAPIView, '/note-delete/<int:note_id>/')
 
 if __name__ == '__main__':
     app.run(debug=True)
