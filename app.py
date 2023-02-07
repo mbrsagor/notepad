305f50a078fdba9b2a5822bd0cdb5f49c6391361
mbrsagor <mbrsagor@gmail.com>
2023-02-07 14:45:19 +0600

    New: Notepadd API create listview done.

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask
-from flask_restful import Resource, Api
+from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
+from flask_restful import Resource, Api
+
+from messages import SUCCESS_MSG, ERROR_MSG, DATA_RENDER_SUCCESS, DATA_RENDER_FAIL
 
 app = Flask(__name__)
 api = Api(app)  # For API service
@@ -14,9 +16,42 @@ class NoteModel(db.Model):
     description = db.Column(db.String(1000))
 
 
-class TestAPIService(Resource):
+class NotePadAPIList(Resource):
+
     def get(self):
-        return {"name": "This is a test API guys."}
+        notepads = NoteModel.query.all()
+        if notepads:
+            notes = []
+            for note in notepads:
+                notepad_details = {
+                    'id': note.id,
+                    'title': note.title,
+                    'description': note.description,
+                }
+                notes.append(notepad_details)
+            response = {
+                'status': 200,
+                'message': DATA_RENDER_SUCCESS,
+                'data': notes
+            }
+            return response
+        else:
+            return {"status": 404, "message": DATA_RENDER_FAIL}
+
+
+class CreateNoteAPIView(Resource):
+    response = {"status": 400, "message": ERROR_MSG}
+
+    def post(self):
+        data = request.get_json()
+        title = data["title"]
+        description = data["description"]
+        notepad = NoteModel(title=title, description=description)
+        db.session.add(notepad)
+        db.session.commit()
+        self.response["status"] = 201
+        self.response["message"] = SUCCESS_MSG
+        return self.response, 201
 
 
 class GetNameAPI(Resource):
@@ -24,7 +59,8 @@ class GetNameAPI(Resource):
         return {"data": f"Hello {name}"}
 
 
-api.add_resource(TestAPIService, '/')
+api.add_resource(NotePadAPIList, '/')
+api.add_resource(CreateNoteAPIView, '/create/')
 api.add_resource(GetNameAPI, '/hello/<string:name>/')
 
 if __name__ == '__main__':
---
+++ messages.py
@@ -0,0 +1,4 @@
+SUCCESS_MSG = "Notepad successfully created."
+ERROR_MSG = "Notepad not created."
+DATA_RENDER_SUCCESS = "Data successfully render"
+DATA_RENDER_FAIL = "No data found at this moment!"
